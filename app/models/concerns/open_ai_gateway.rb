5b0c908bb4e1ff129b11b0a754df1816325b9a43
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-03-23 03:23:33 +0530

    feat: add gateway

---
+++ app/models/concerns/open_ai_gateway.rb
@@ -0,0 +1,23 @@
+require 'json'
+require 'httparty'
+
+module OpenAiGateway
+  def get_open_ai_completion(prompt, model = 'gpt-3.5-turbo')
+    headers = {
+      'Content-Type' => 'application/json',
+      'Authorization' => "Bearer #{ENV['OPENAI_API_KEY']}"
+    }
+
+    data = {
+      model: model,
+      temperature: 0.2,
+      messages: [{
+        role: 'user',
+        content: prompt
+      }]
+    }
+
+    response = HTTParty.post('https://api.openai.com/v1/chat/completions', headers: headers, body: data.to_json)
+    response.body
+  end
+end

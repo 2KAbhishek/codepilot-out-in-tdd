0a437a383a3676fee5758179229c84e4e73b7b65
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-03-23 12:50:11 +0530

    refactor: integration test

--- spec/controllers/completions_controller_spec.rb
+++ spec/controllers/completions_controller_spec.rb
@@ -4,30 +4,8 @@ include WebMock::API
 
 describe CompletionsController do
   describe 'GET #ask' do
-    let(:mock_response) do
-      "{\"id\":\"chatcmpl-6wz0dMspx8QiyGpGIgDpzXowivbas\",
-    \"object\":\"chat.completion\",
-    \"created\":1679515563,
-    \"model\":\"gpt-3.5-turbo-0301\",
-    \"usage\":{\"prompt_tokens\":14,
-    \"completion_tokens\":44,
-    \"total_tokens\":58},
-    \"choices\":[{\"message\":{\"role\":\"assistant\",
-    \"content\":\"\\n\\nIn Ruby you can use the following code:\\n\\n```ruby\\nputs \\\"Hello, World!\\\"\\n```\\n\\n\"},
-    \"finish_reason\":\"stop\",
-    \"index\":0}]}\n"
-    end
-
-    let(:query) { 'how to write hello world in ruby' }
-
-    it 'should return status code 200' do
-      get :ask, params: { query: query }, as: :json
-      expect(response.status).to eql(200)
-    end
-
-    it 'response should contain proper response' do
-      WebMock.enable!
-
+    WebMock.enable!
+    before :each do
       stub_request(:post, 'https://api.openai.com/v1/chat/completions')
         .with(
           body: '{"model":"gpt-3.5-turbo","temperature":0.2,"messages":[{"role":"user","content":"how to write hello world in ruby"}]}',
@@ -40,8 +18,27 @@ describe CompletionsController do
           }
         )
         .to_return(status: 200, body: mock_response, headers: {})
+    end
 
+    let(:mock_response) do
+      "{\"id\":\"chatcmpl-6wz0dMspx8QiyGpGIgDpzXowivbas\",
+      \"object\":\"chat.completion\",
+      \"created\":1679515563,
+      \"model\":\"gpt-3.5-turbo-0301\",
+      \"usage\":{\"prompt_tokens\":14,
+      \"completion_tokens\":44,
+      \"total_tokens\":58},
+      \"choices\":[{\"message\":{\"role\":\"assistant\",
+      \"content\":\"\\n\\nIn Ruby you can use the following code:\\n\\n```ruby\\nputs \\\"Hello, World!\\\"\\n```\\n\\n\"},
+      \"finish_reason\":\"stop\",
+      \"index\":0}]}\n"
+    end
+
+    let(:query) { 'how to write hello world in ruby' }
+
+    it 'response should contain proper response' do
       get :ask, params: { query: query }, as: :json
+      expect(response.status).to eql(200)
       expect(JSON.parse(response.body)['code']).to have_attributes(size: (be > 0))
       expect(JSON.parse(response.body)['info']).to have_attributes(size: (be > 0))
     end
